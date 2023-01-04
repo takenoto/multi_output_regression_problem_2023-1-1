b75c012ba030b4e182250f04d649e529010655f1
takenoto <mendesrenan5@gmail.com>
2023-01-04 14:20:36 -0300

    adição de #todos para uso futuro

---
+++ decision_tree/decision_tree_adsorption.py
@@ -0,0 +1,7 @@
+
+# TODO faz uma árvore de decisão pra determinar
+# QUAL RESINA/GRUPO FUNCIONAL USAR PRA QUAL COMPONENTE
+# O problema é quw o output é variável demais, acho q n rola.
+# O que quero é algo do tipo "pra variável X, quais são as possibilidades"
+# No caso X é a substância de interesse
+# e as possibilidades são pH e afins, resina, condições de operação...
